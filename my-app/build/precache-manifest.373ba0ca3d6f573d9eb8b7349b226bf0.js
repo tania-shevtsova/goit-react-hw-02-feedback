d32a7931c4067c9405db4f59405c70ba0443d993
Tania <taniameoow@gmail.com>
2020-02-05 09:29:46 +0200

    deploy

---
+++ my-app/build/precache-manifest.373ba0ca3d6f573d9eb8b7349b226bf0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5ed300ae7f8954c6ff1c3b4f4528495",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "2403d0440123483b27be",
+    "url": "/goit-react-hw-02-feedback/static/js/2.9e0668a7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/goit-react-hw-02-feedback/static/js/2.9e0668a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce00a13a9bb49c6609dc",
+    "url": "/goit-react-hw-02-feedback/static/js/main.8c5267f3.chunk.js"
+  },
+  {
+    "revision": "a9008001ac811cd62d60",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.58c7c2ad.js"
+  }
+]);
